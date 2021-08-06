f86a4d5fd18700171ad23a625ce166de3b379e0d
unknown <tyler.r.roll@gmail.com>
2021-08-06 16:36:47 -0600

    when and validate

--- app.js
+++ app.js
@@ -1,14 +1,152 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js')
+const inquirer = require('inquirer')
 
-const profileDataArgs = process.argv.slice(2);
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        }
+        else {
+          console.log('Please enter your name!')
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username',
+      validate: userName => {
+        if (userName) {
+          return true;
+        }
+        else {
+          console.log('Please enter your name!')
+          return false;
+        }
+      }
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAbout',
+      message: 'Would you like to enter some information about yourself for an "About" section?',
+      default: true
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:',
+      when: ({ confirmAbout }) => {
+        if (confirmAbout) {
+          return true
+        }
+        else {
+          return false
+        }
+      }
+    }
+  ]);
+};
+const promptProject = portfolioData => {
+  console.log(`
+    =================
+    Add a New Project
+    =================
+    `);
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of your project?',
+      validate: project => {
+        if (project) {
+          return true;
+        }
+        else {
+          console.log('Please enter the name!')
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of the project (Required)',
+      validate: description => {
+        if (description) {
+          return true;
+        }
+        else {
+          console.log('Please enter a description!')
+          return false;
+        }
+      }
+    },
+    {
+      type: 'checkbox',
+      name: 'languages',
+      message: 'What did you build this project with? (Check all that apply)',
+      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Enter the GitHub link to your project. (Required)',
+      validate: link => {
+        if (link) {
+          return true;
+        }
+        else {
+          console.log('Please enter your link!')
+          return false;
+        }
+      }
+    },
+    {
+      type: 'confirm',
+      name: 'feature',
+      message: 'Would you like to feature this project?',
+      default: false
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAddProject',
+      message: 'Would you like to enter another project?',
+      default: false
+    }
+  ])
+    .then(projectData => {
+      portfolioData.projects.push(projectData);
+      if (projectData.confirmAddProject) {
+        return promptProject(portfolioData);
+      }
+      else {
+        return portfolioData;
+      }
+    })
+};
 
-const [name, github] = profileDataArgs;
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    console.log(portfolioData)
+  })
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js')
+
+// const pageHtml = generatePage(name, github)
 
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//   if (err) throw new Error(err);
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
